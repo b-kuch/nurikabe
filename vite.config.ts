4cbf89e54219f2e1ae312f83130a3d2af7209083
Bartłomiej Kucharczyk <bartlomiej.kucharczyk@sollers.eu>
2026-09-18 13:53:55 +0200

    fix cf build

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   worker: { format: 'es' },
   test: { include: ['tests/**/*.test.ts'], testTimeout: 60000 },
+  plugins: []
 });
